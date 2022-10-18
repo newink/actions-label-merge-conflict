a59138100ac30bfd918924b5c3235d28b103d5ad
Sergey Ivanov <sivanov@unison.team>
2022-10-18 20:43:40 +0300

    Fix tg markdown

--- sources/main.ts
+++ sources/main.ts
@@ -208,7 +208,7 @@ query openPullRequests($owner: String!, $repo: String!, $after: String, $baseRef
 
 					const message = context.telegramMessageTemplate.replace('{tg_login}', telegramLogin)
 						.replace('{pr_link}', pullRequest.permalink)
-					await bot.telegram.sendMessage(context.telegramChatId, message, {parse_mode: 'MarkdownV2'})
+					bot.telegram.sendMessage(context.telegramChatId, message, {parse_mode: 'MarkdownV2'})
 				} else {
 					info(`Telegram notifications disabled`)
 				}
